e8ab80f4f12235b700e5f2f4a3cd6d637e8ce82d
Akej24 <aleks.skotarek.private@gmail.com>
2023-11-27 14:01:25 +0100

    added postgresql sql code version

--- src/main/resources/migrations/01-create-account-credentials-table.sql
+++ src/main/resources/migrations/01-create-account-credentials-table.sql
@@ -2,7 +2,7 @@
 --changeset aleks:1
 
 CREATE TABLE account (
-    account_id INT AUTO_INCREMENT NOT NULL,
+    account_id INT AUTO_INCREMENT NOT NULL, /* PostgreSQL: "account_id SERIAL PRIMARY KEY," */
     email VARCHAR(255) UNIQUE NOT NULL,
     encoded_password VARCHAR(255) NOT NULL,
     first_name VARCHAR(255) NOT NULL,
